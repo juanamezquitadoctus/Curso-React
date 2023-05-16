c8594c0afd721246ebaa1e996455fb854c578c7a
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-05-16 12:11:30 -0500

    43 y 44

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
 import { CounterApp } from './CounterApp';
+// import { CounterApp2 } from './CounterApp2';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp subTitle={ 123 } />
-        <CounterApp />
+        {/* <FirstApp subTitle={ 123 } /> */}
+        <CounterApp value={ 456 } />
+        {/* <CounterApp2 value={ 789 } /> */}
     </React.StrictMode>
 )
