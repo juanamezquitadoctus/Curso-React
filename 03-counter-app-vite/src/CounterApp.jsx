26bbc064f02c2a3b0397909eca14f255dddbdf8d
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-05-18 12:26:28 -0500

    Seccion 4

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
 
+    const [ counter, setCounter ] = useState( value );
+
     const handleAdd = () =>{
         // console.log(event);
-        console.log('+1');
-        value = 1000;
+        setCounter( counter + 1 );
+        // setCounter( (c) => c + 1 );
     }
+
+    const handleReduce = () =>{
+        setCounter( counter - 1 );
+    }
+
+    const handleReset = () => setCounter( value );
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <h2> { counter } </h2>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReduce }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
         </>
     );
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -10,7 +10,7 @@ import './styles.css';
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         {/* <FirstApp subTitle={ 123 } /> */}
-        <CounterApp value={ 456 } />
+        <CounterApp value={ 20 } />
         {/* <CounterApp2 value={ 789 } /> */}
     </React.StrictMode>
 )
