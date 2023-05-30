010bcf999f64b5988b1f9f4333d137090497250c
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-05-30 12:13:10 -0500

    59

---
+++ 03-counter-app-vite/tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,46 @@
+import { getHeroeByIdAsync } from '../../src/base-pruebas/09-promesas';
+
+describe('Pruebas en 09-promesas', () => {
+
+    test('getHeroeByIdAsync debe de retornar un héroe', (done) => {
+
+        const id = 1;
+
+        getHeroeByIdAsync( id )
+            .then( hero => {
+
+                expect(hero).toEqual({
+                    id: 1,
+                    name: 'Batman',
+                    owner: 'DC'
+                });
+
+                done();
+
+            });
+
+    });
+
+    test('getHeroeByIdAsync debe de obtener un error si héroe no exste', (done) => {
+
+        const id = 100;
+
+        getHeroeByIdAsync( id )
+            .then( hero => {
+
+                expect(hero).toBeFalsy();
+
+                done();
+
+            })
+            .catch( error => {
+
+                expect(error).toBe('No se pudo encontrar el héroe');
+
+                done();
+
+            });
+
+    });
+
+});
