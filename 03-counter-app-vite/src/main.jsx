183db2a23262ef19920c015aa45c7effdc0301dd
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-06-08 18:19:29 -0500

    65 y 66

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -10,8 +10,8 @@ import './styles.css';
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         {/* <FirstApp subTitle={ 123 } /> */}
-        {/* <CounterApp value={ 20 } /> */}
+        <CounterApp value={ 20 } />
         {/* <CounterApp2 value={ 789 } /> */}
-         <FirstApp title='Hola, soy Vegeta' />
+         {/* <FirstApp title='Hola, soy Vegeta' /> */}
     </React.StrictMode>
 )
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -3,26 +3,43 @@ import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
 
-    test('Debe de hacer match con el snapshot', () => {
+    // test('Debe de hacer match con el snapshot', () => {
 
-        const title ='Hola, soy Goku';
-        const { container } = render( <FirstApp title={ title } /> );
+    //     const title ='Hola, soy Goku';
+    //     const { container } = render( <FirstApp title={ title } /> );
 
-        expect( container ).toMatchSnapshot();
+    //     expect( container ).toMatchSnapshot();
 
-    });
+    // });
 
     test('debe de mostrar el título en un h1', () => {
 
         const title = 'Hola, soy Goku';
-        const { container, getByText } = render( <FirstApp title={ title } /> );
+        const { container, getByText, getByTestId } = render( <FirstApp title={ title } /> );
 
         expect( getByText(title) ).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
+        // const h1 = container.querySelector('h1');
 
-        expect(h1.innerHTML).toBe( title );
+        // expect(h1.innerHTML).toBe( title );
 
-    })
+        expect( getByTestId('test-title').innerHTML ).toContain(title)
+
+    });
+
+    test('Debe de mostrar el subtitulo enviado por props', () => {
+
+        const title = 'Hola, soy Goku';
+        const subTitle = 'Soy un subtitulo';
+        const { getAllByText } = render( 
+            <FirstApp
+                title={ title }
+                subTitle={ subTitle }
+            />
+        );
+
+        expect( getAllByText(subTitle).length ).toBe(2);
+    
+    });
 
 });
