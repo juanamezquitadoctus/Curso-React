0743a8ddf669c74780e1693c2cff5e6622a56a32
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-06-09 19:28:48 -0500

    69

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -23,7 +23,7 @@ export const CounterApp = ({ value }) => {
             <h2> { counter } </h2>
             <button onClick={ handleAdd }>+1</button>
             <button onClick={ handleReduce }>-1</button>
-            <button onClick={ handleReset }>Reset</button>
+            <button aria-label='btn-reset' onClick={ handleReset }>Reset</button>
         </>
     );
 }
