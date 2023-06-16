cdfae1326c158c13d03f9dbd0acaf4fe21c38e18
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-06-15 19:34:05 -0500

    75 y 76

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+  const [ categories, setcategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+    // categories.push( 'Naruto' );
+    // setcategories([
+    //   ...categories,
+    //   'Naruto'
+    // ]);
+    setcategories( cat => [ 'Naruto', ...cat  ] );
+  }
+
+  console.log(categories);
+
   return (
    <>
     {/* Titulo */}
     <h1>GifExpertApp</h1>
+
     {/* Input */}
+
     {/* Listado de Gif */}
+    <button onClick={ onAddCategory }>Agregar</button>
+    <ol>
+      {
+        categories.map( category => {
+          return <li key={ category }>{ category }</li>
+        } )
+      }
+      {/* <li>ABC</li> */}
+    </ol>
       {/* Gif Item */}
    </>
   )
