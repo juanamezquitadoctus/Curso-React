49246339a3c49796f537d533207db1bf482eae60
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-07-17 19:04:08 -0500

    Demostración de Producción Rapida B

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-
-  const [counter, setCounter] = useState(10);
 
   useEffect( () => {
     getGifs(category);
