9f31e0ca3f1e28323313e91fbdb9178e90d7dbed
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-07-21 19:29:57 -0500

    className - Clases de CSS

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
@@ -18,13 +19,17 @@ export const GifGrid = ({ category }) => {
     <>
         <h3>{ category }</h3>
 
-        <ol>
+        <div className="card-grid">
           {
-            images.map( ({ id,title }) => (
-              <li key={ id }>{ title }</li>
+            images.map( ( image ) => (
+              <GifItem
+                key={ image.id }
+                { ...image }
+                {  }
+              />
             ))
           }
-        </ol>
+        </div>
     </>
   )
 }
