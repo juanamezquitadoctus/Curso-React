7790d207b92e5181551f84d7aab0bab8dc0a2f8d
juan-amezquita <juan.amezquita@doctus.com.co>
2023-08-17 18:21:05 -0500

    useFetch + useCounter

--- 05-hook-app/src/03-examples/MultipleCustomHook.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHook.jsx
@@ -3,12 +3,35 @@ import { useFetch } from "../hooks/useFetch"
 export const MultipleCustomHook = () => {
     const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
-    console.log({data, isLoading, hasError});
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
             <h1>BreakingBad Quotes</h1>
             <hr />
+
+            {
+                isLoading
+                    ? (
+
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+
+                    )
+                    : (
+
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-1">{ quote }</p>
+                            <footer className="blockquote-footer">{ author }</footer>
+                        </blockquote>
+                        
+                    )
+            }
+
+            <button className="btn btn-primary">
+                Next quote
+            </button>
         </>
     )
 }
