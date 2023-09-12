00778f6ceab4d441a030223b3f7946938938f6bc
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-09-11 19:44:04 -0500

    Creando el cascaron de la lista de TODOs y Tarea: Crear componentes y emitir eventos

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -9,23 +9,50 @@ const initialState = [
     },
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false,
     },
 ]
 
 export const TodoApp = () => {
-    const [todos, dispatch] = useReducer( todoReducer, initialState )
+    const [todos, dispatch] = useReducer( todoReducer, initialState );
+
+    const handleNewTodo = ( todo ) => {
+        console.log({ todo });
+    }
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp: 10, <small>Pendientes: 2</small></h1>
             <hr />
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    {/* TodoList */}
+                    <ul className="list-group">
+                        {
+                            todos.map( todo => (
+                                // TodoItem...
+                                <li key={ todo.id } className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item 1</span>
+                                    <button className="btn btn-danger">Borrar</button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    {/* Fin TodoList */}
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    {/* TodoAdd onNewTodo( todo ) */}
+                    {/* { id: new Date()..., description: '', done: false } */}
+                    <form>
+                        <input type="text" placeholder="Que hay que hacer?" className="form-control" />
+                        <button type="submit" className="btn btn-outline-primary mt-1">Agregar</button>
+                    </form>
+                    {/* Fin TodoAdd */}
+                </div>
+            </div>
         </>
     )
 }
