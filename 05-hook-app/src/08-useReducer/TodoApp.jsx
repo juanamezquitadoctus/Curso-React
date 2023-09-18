d8cfb1b813925b5e8550a6c2eaa362cabb756641
SOLUDOCTUS\juan.amezquita <juan.amezquita@doctus.com.co>
2023-09-18 18:05:29 -0500

    Borrar un TODO

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -36,6 +36,13 @@ export const TodoApp = () => {
 
         dispatch( action );
     }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
     
     return (
         <>
@@ -43,7 +50,7 @@ export const TodoApp = () => {
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={ todos } />
+                    <TodoList todos={ todos } onDeleteTodo={ handleDeleteTodo } />
                 </div>
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
